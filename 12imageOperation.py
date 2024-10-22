3085cd41b76bc26cc9a55bdb0264d6341e2fe707
akashverma55 <akvakv150@gmail.com>
2024-10-23 00:56:32 +0530

    new images with different color channels

--- 12imageOperation.py
+++ 12imageOperation.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Tanjiro.jpg")
+img = cv2.imread("japan.jpg")
 print("Shape == ",img.shape)
 print("No of Pixels == ",img.size)
 print("Datatype == ",img.dtype)
@@ -10,20 +10,25 @@ img = cv2.resize(img,(600,400))
 
 # this method split this image into three color channels blue, green and red
 b, g, r =cv2.split(img)
-cv2.imshow("blue",b)
-cv2.imshow("green",g)
-cv2.imshow("red",r)
+# cv2.imshow("blue",b)
+# cv2.imshow("green",g)
+# cv2.imshow("red",r)
 
 cv2.imshow("original",img)
+black = np.zeros_like(r)
+mr1 = cv2.merge((black,black, r))
+cv2.imshow("r",mr1)
 
-mr1 = cv2.merge((r,g,b))
-cv2.imshow("rgb",mr1)
+mr2 = cv2.merge((black,g,black))
+cv2.imshow("g",mr2)
 
-mr2 = cv2.merge((g,b,r))
-cv2.imshow("gbr",mr2)
+mr3 = cv2.merge((b,black,black))
+cv2.imshow("b",mr3)
+# mr2 = cv2.merge((g,b,r))
+# cv2.imshow("gbr",mr2)
 
-mr3 = cv2.merge((g,r,b))
-cv2.imshow("grb",mr3)
+# mr3 = cv2.merge((g,r,b))
+# cv2.imshow("grb",mr3)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
