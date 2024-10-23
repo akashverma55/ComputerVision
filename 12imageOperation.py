5c987a3d9a6e7b42429931f0704cdfadd2e299e7
akashverma55 <akvakv150@gmail.com>
2024-10-23 10:12:40 +0530

    modified

--- 12imageOperation.py
+++ 12imageOperation.py
@@ -10,9 +10,6 @@ img = cv2.resize(img,(600,400))
 
 # this method split this image into three color channels blue, green and red
 b, g, r =cv2.split(img)
-# cv2.imshow("blue",b)
-# cv2.imshow("green",g)
-# cv2.imshow("red",r)
 
 cv2.imshow("original",img)
 black = np.zeros_like(r)
@@ -24,11 +21,12 @@ cv2.imshow("g",mr2)
 
 mr3 = cv2.merge((b,black,black))
 cv2.imshow("b",mr3)
-# mr2 = cv2.merge((g,b,r))
-# cv2.imshow("gbr",mr2)
 
-# mr3 = cv2.merge((g,r,b))
-# cv2.imshow("grb",mr3)
+mr2 = cv2.merge((g,b,r))
+cv2.imshow("gbr",mr2)
+
+mr3 = cv2.merge((g,r,b))
+cv2.imshow("grb",mr3)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
