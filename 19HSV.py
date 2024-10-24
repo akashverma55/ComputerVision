9a8079f53e991629e6788810ab1fec249d0a57b1
akashverma55 <akvakv150@gmail.com>
2024-10-25 00:32:55 +0530

    HSV operation

---
+++ 19HSV.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("OIP.jpeg")
+
+while True:
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    upper = np.array([130,225,225])
+    lower = np.array([110,50,50])
+    # creating mask
+    mask = cv2.inRange(hsv,lower,upper)
+    # Filter mask from image with bitwise operation
+    res = cv2.bitwise_and(img,img,mask=mask)
+
+    cv2.imshow("IMG",img)
+    cv2.imshow("Mask",mask)
+    cv2.imshow("Res",res)
+
+    k=cv2.waitKey(1)
+    if k==27:
+        break
+cv2.destroyAllWindows()
