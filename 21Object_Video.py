03800007e087cd5186f007ddb1412fa6f3509e1d
akashverma55 <akvakv150@gmail.com>
2024-10-25 15:35:38 +0530

    Colorful Object Detection Using HSV in webcam

---
+++ 21Object_Video.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+def nothing(x):
+    pass
+
+cv2.namedWindow("Color_Adjustments")
+
+cv2.createTrackbar("Lower_H","Color_Adjustments",0,255,nothing)
+cv2.createTrackbar("Lower_S","Color_Adjustments",0,255,nothing)
+cv2.createTrackbar("Lower_V","Color_Adjustments",0,255,nothing)
+
+cv2.createTrackbar("Upper_H","Color_Adjustments",255,255,nothing)
+cv2.createTrackbar("Upper_S","Color_Adjustments",255,255,nothing)
+cv2.createTrackbar("Upper_V","Color_Adjustments",255,255,nothing)
+
+while True:
+    _,frame = cap.read()
+    frame = cv2.resize(frame,(500,300))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    l_h = cv2.getTrackbarPos("Lower_H","Color_Adjustments")
+    l_s = cv2.getTrackbarPos("Lower_S","Color_Adjustments")
+    l_v = cv2.getTrackbarPos("Lower_V","Color_Adjustments")
+
+    u_h = cv2.getTrackbarPos("Upper_H","Color_Adjustments")
+    u_s = cv2.getTrackbarPos("Upper_S","Color_Adjustments")
+    u_v = cv2.getTrackbarPos("Upper_V","Color_Adjustments")
+
+    lower_bound = np.array([l_h,l_s,l_v])
+    upper_bound = np.array([u_h,u_s,u_v])
+
+    mask = cv2.inRange(hsv,lower_bound,upper_bound)
+    res = cv2.bitwise_and(frame,frame,mask=mask)
+
+    cv2.imshow("IMG",frame)
+    cv2.imshow("Mask",mask)
+    cv2.imshow("Res",res)
+
+    k=cv2.waitKey(1)
+    if k==27:
+        break
+
+cv2.destroyAllWindows()
